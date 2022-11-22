4f7a04ac41278bb2387d783b7f5da69088d927ae
Kongpanat <112373787+Kongpanat@users.noreply.github.com>
2022-11-22 19:32:54 +0700

    Add files via upload

--- home.php
+++ home.php
@@ -1,10 +1,28 @@
-<?php require_once('connect.php');
-session_start();
-if (isset($_SESSION['admin_id'])) {
-    $admin_id = $_SESSION['admin_id'];
-}
-?>
 <!DOCTYPE html>
+
+<?php
+require_once('connect.php');
+session_start();
+if (isset($_SESSION['loginemail']) && isset($_SESSION['loginemail'])) {
+    $loginemail = $_SESSION['loginemail'];
+    $loginpassword = $_SESSION['loginpassword'];
+} else {
+    header('location:login.php');
+}
+//identify account info. that login
+$q = "SELECT Fname,Lname,Branch_ID FROM staff WHERE Email ='$loginemail' AND Password='$loginpassword'";
+if ($result = $mysqli->query($q)) {
+    while ($row = $result->fetch_array()) {
+        if (isset($row[2])) {
+            $AccFname = $row[0];
+            $AccLname = $row[1];
+            $AccBranch = $row[2];
+        }
+    }
+} else {
+    echo 'Query error: ' . $mysqli->error;
+} ?>
+
 <html lang="en">
 
 <head>
@@ -38,30 +56,21 @@ if (isset($_SESSION['admin_id'])) {
     <div class="boxed-page">
         <nav id="gtco-header-navbar" class="navbar navbar-expand-lg py-4">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="home.php">
-                    <span class="lnr lnr-moon"></span>
-                </a>
+                <p style="font-size:18px;"><?php echo ('<b>User</b> : ' . $AccFname . '&emsp;' . $AccLname . '&emsp;<b>Branch</b> : ' . $AccBranch); ?></p>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-nav-header" aria-controls="navbar-nav-header" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="lnr lnr-menu"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbar-nav-header">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="home.php">Home</a>
+                            <a class="nav-link" href="home.php">Home&emsp;</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="profile.php">Profile</a>
+                            <a class="nav-link" href="c_register.php">Customer Register&emsp;</a>
                         </li>
-                        <?php if (!isset($admin_id)) : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="inventory.php">Inventory</a>
-                            </li>
-                        <?php endif ?>
-                        <?php if (isset($admin_id)) : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="add_film.php">Add Film</a>
-                            </li>
-                        <?php endif ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="p_register.php">Package Register&emsp;</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="logout.php">Logout</a>
                         </li>
@@ -74,81 +83,54 @@ if (isset($_SESSION['admin_id'])) {
                 <div class="section-content">
                     <!-- Section Title -->
                     <div class="title-wrap">
-                        <h1 class="display-2 mb-4">Search Film</h1><br>
+                        <h1 class="display-2 mb-4">HOMEPAGE</h1><br>
                     </div>
                     <!-- End of Section Title -->
                     <div class="row">
                         <!-- Contact Form Holder -->
-                        <div class="col-md-8 offset-md-2 contact-form-holder mt-4">
-
+                        <div class="col-md-15 contact-form-holder mt-4">
                             <form method="post" name="login-form" action="">
-
-                                <div class="row">
-                                    <div class="col-md-12 form-input">
-                                        <select class="form-control" id="title" name="title">
-                                            <option value="" disabled selected>Film Title</option>
-                                            <!--Do php-->
-                                            <?php
-                                            $q = "SELECT * FROM film,film_genre WHERE film.film_id=film_genre.film_id ";
-                                            $result = $mysqli->query($q);
-                                            if (!$result) {
-                                                echo "Select failed. Error: " . $mysqli->error;
-                                                return false;
-                                            }
-                                            while ($row = $result->fetch_array()) {
-                                            ?>
-                                                <option value="<?= $row['title'] ?>"><?= $row['title'] ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                    <br>
-                                    <br>
-                                    <div class="col-md-12 form-btn text-center">
-                                        <input class="btn btn-block btn-secondary btn-red" type="submit" name="submit" value="SEARCH">
-                                    </div>
-                                    <?php
-                                    if (isset($_POST['submit'])) {
-                                        if (!empty($_POST['title'])) {
-                                            $selected = $_POST['title'];
-                                            $x = 'film.php?title=' . $selected;
-                                            echo ("<script LANGUAGE='JavaScript'>
-														window.location.href='$x';
-														</script>");
-                                        } else {
-                                            echo 'Please select film title.';
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                                <br>
-                                <h5>All films</h5>
+                                <h5>Tracking Info</h5>
                                 <br>
                                 <!--Begin Table-->
                                 <table style="text-align: center; border-collapse: collapse; border: 1px solid black; table-layout: fixed; width: 100%;">
                                     <tr>
-                                        <th style="border: 1px solid black">Title</th>
-                                        <th style="border: 1px solid black">Release Year</th>
-                                        <th style="border: 1px solid black">Genre</th>
-                                        <th style="border: 1px solid black">Length</th>
-                                        <th style="border: 1px solid black">Rental Rate</th>
+                                        <th style="border: 1px solid black">Sending ID</th>
+                                        <th style="border: 1px solid black">Sender</th>
+                                        <th style="border: 1px solid black">Customer ID (Sender)</th>
+                                        <th style="border: 1px solid black">Receiver</th>
+                                        <th style="border: 1px solid black">Customer ID (Receiver)</th>
+                                        <th style="border: 1px solid black">Sending Type</th>
+                                        <th style="border: 1px solid black">Pick-up Time</th>
+                                        <th style="border: 1px solid black">Delivered Time</th>
+                                        <th style="border: 1px solid black">Status</th>
+                                        <th style="border: 1px solid black">Edit</th>
                                     </tr>
                                     <!--Do php in tr-->
                                     <?php
-                                    $q = "SELECT * FROM film,film_genre WHERE film.film_id=film_genre.film_id ";
-                                    $result = $mysqli->query($q);
-                                    if (!$result) {
-                                        echo "Select failed. Error: " . $mysqli->error;
-                                        return false;
+                                    //Fill Tracking Info.
+                                    $q = 'SELECT x.sending_ID,c.Fname,c.Lname,s.customer_ID,r.Fname,r.Lname,r.customer_ID,x.sending_type,x.pickup_time,x.delivered_time,x.status From sending x,sender s,receiver r,customer c WHERE s.sender_ID=x.sender_ID  AND r.receiver_ID=x.receiver_ID AND s.customer_ID=c.customer_ID;';
+                                    if ($result = $mysqli->query($q)) {
+                                        while ($row = $result->fetch_array()) {
+                                            echo "<tr>";
+                                            echo "<td style='border: 1px solid black'>" . $row[0] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[1] . "&emsp;" . $row[2] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[3] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[4] . "&emsp;" . $row[5] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[6] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[7] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[8] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[9] . "</td>";
+                                            echo "<td style='border: 1px solid black'>" . $row[10] . "</td>";
+                                    ?>
+                                            <td style='border: 1px solid black'><a href='edit_home.php?sendingid=<?= $row[0] ?>&pickup=<?= $row[8] ?>&delivered=<?= $row[9] ?>'>EDIT</a></td>
+                                    <?php
+                                            echo "</tr>";
+                                        }
+                                    } else {
+                                        echo 'Query error: ' . $mysqli->error;
                                     }
-                                    while ($row = $result->fetch_array()) { ?>
-                                        <tr>
-                                            <td><a href='film.php?title=<?= $row['title'] ?>'><?= $row['title'] ?></a></td>
-                                            <td><?= $row['release_year'] ?> </td>
-                                            <td><?= $row['genre_name'] ?></td>
-                                            <td><?= $row['length'] ?></td>
-                                            <td><?= $row['rental_rate'] ?></td>
-                                        </tr>
-                                    <?php } ?>
+                                    ?>
                                 </table>
                                 <!--End Table-->
                             </form>
@@ -158,12 +140,8 @@ if (isset($_SESSION['admin_id'])) {
             </div>
         </section>
         <footer class="mastfoot mb-3 bg-white py-4 border-top">
-            <div class="inner container">
-                <div class="row">
-                    <div class="col-md-6 d-flex align-items-center justify-content-md-start justify-content-center">
-                        <p class="mb-0">Project CSS326 Movie Rental System</p>
-                    </div>
-                </div>
+            <div class="row">
+                <p style="font-size:20px">&emsp;&emsp;CSS 325 Database Systems : Shipping Management System</p>
             </div>
         </footer>
     </div>
@@ -183,5 +161,19 @@ if (isset($_SESSION['admin_id'])) {
     <script src="js/app.min.js "></script>
     <script src="//localhost:35729/livereload.js"></script>
 </body>
+
+<?php
+if (isset($_POST['uphomesub'])) {
+    $sid = $_POST["sendingid"];
+    $pickup = $_POST["pickuptime"];
+    $delivered = $_POST["deliveredtime"];
+    $status = $_POST["status"];
+    $q = "UPDATE sending SET pickup_time='$pickup',delivered_time='$delivered',status='$status' WHERE sending_id='$sid' ";
+    $result = $mysqli->query($q);
+    if (!$mysqli->query($q)) {
+        echo "UPDATE failed. Error:" . $mysqli->error;
+    }
+}
+?>
 
 </html>
